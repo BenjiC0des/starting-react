db607642ddc996c93df84a7ec9eea54b8306e72e
benbarre <benbarre@publicisgroupe.net>
2022-12-18 17:17:20 -0500

    half way through #5

--- src/App.js
+++ src/App.js
@@ -4,35 +4,37 @@ import PropTypes from 'prop-types';
 import './App.css';
 import pokemon from './pokemon.json';
 
-const ListLength = 10;
+//variable & constants
+const listLength = 10;
 
-const PokemonRow = ({ pokemon })=>(
+//components
+const PokemonRow = ({pokemon})=>(
     <tr>
         <td>{pokemon.name.english}</td>
         <td>{pokemon.type.join(', ')}</td>
     </tr>
 )
 
-PokemonRow.propTypes = {
+//Property Types
+PokemonRow.propTypes = ({
     pokemon: PropTypes.shape({
         name: PropTypes.shape({
             english: PropTypes.string,
         }),
         type: PropTypes.arrayOf(PropTypes.string),
     })
-}
-
+})
 
 function App() {
+    const [filter, filterSet] = React.useState('');
   return (
-      <div
-          style={{
-              width:520,
-              textAlign:'center',
-              margin:'auto',
-          }}
-      >
-          <h1 className="h1">Pokemon Search</h1>
+      <div style={{
+          width:'100%',
+          maxWidth:520,
+          margin:'auto'
+      }}>
+          <h1 className='h1'>Pokémon Search</h1>
+          <input value={filter} onChange={(evt)=>filterSet(evt.target.value)}/>
           <table>
               <thead>
               <tr>
@@ -41,7 +43,9 @@ function App() {
               </tr>
               </thead>
               <tbody>
-              {pokemon.slice(0,ListLength).map((pokemon)=>(
+              {pokemon
+                  .filter((pokemon)=>pokemon.name.english.toLowerCase().includes(filter.toLowerCase()))
+                  .slice(0,listLength).map((pokemon)=>(
                   <PokemonRow pokemon={pokemon} key={pokemon.id} />
               ))}
               </tbody>
